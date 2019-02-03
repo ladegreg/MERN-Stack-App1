4de005e63758ba11363e021488813f6188b8b4e5
ladegreg <grzegorzlade@gmail.com>
2019-02-03 07:12:18 +0100

    ok
    3

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB OK...'))
   .catch(err => console.log(err));
 
